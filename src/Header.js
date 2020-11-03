5e70fcadd6e486be371d7890ec5593599de79738
Paresh Wadhwani <parresh77@gmail.com>
2020-11-04 00:55:40 +0530

    Yet Another Initial Commit

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ',/Header.css';
+
+function Header()
+{
+    return
+        <div className = 'header'>
+            <div className = 'header__left'>
+
+            </div>
+            <div className = 'header__search'>
+
+            </div>
+            <div className = 'header__right'>
+
+            </div>
+        </div>
+}
+
+export default Header
