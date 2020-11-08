5a94f39b60ec5c1100c79e4ff989ab2e9ca122cc
parresh77 <parresh77@gmail.com>
2020-11-09 00:57:51 +0530

    Completed static design of the main page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Header from './Header.js';
 import Sidebar from './Sidebar.js';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className = 'app__body'>
-        <Sidebar />
-        {
-          // router
-        }
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<div className = 'app__body'>
+			<Sidebar />
+			{
+				// router
+			}
+			</div>
+		</div>
+	);
 }
 
 export default App;
