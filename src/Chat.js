86a7d515e190fd5eafb6a0ffd18ea0fb1227acb4
parresh77 <parresh77@gmail.com>
2020-11-16 19:20:33 +0530

    Add chat header

--- src/Chat.js
+++ src/Chat.js
@@ -2,25 +2,27 @@ import React from 'react';
 import './Chat.css';
 import { useParams } from  'react-router-dom';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
+import  InfoOutlinedIcon  from '@material-ui/icons/InfoOutlined';
 
 function Chat() {
-    const { roomId } = useParams();
-    return (
-        <div className = 'chat'>
-            <h2>this is {roomId}</h2>
-            <div className = 'chat__header'>
+	const { roomId } = useParams();
+	return (
+		<div className = 'chat'>
+			<div className = 'chat__header'>
                 <div className = 'chat__headerLeft'>
-                    <div className = 'chat__headerLeft'>
-                        <h4 className = 'chat__channelName'># General</h4>
+                    <h4 className = 'chat__channelName'>
+                        <strong># General</strong>
                         <StarBorderOutlinedIcon />
-                    </div>
+                    </h4>
                 </div>
                 <div className = 'chat__headerRight'>
-                    a
+                    <p>
+                        <InfoOutlinedIcon /> Details
+                    </p>
                 </div>
             </div>
-        </div>
-    )
+		</div>
+	)
 }
 
 export default Chat;
