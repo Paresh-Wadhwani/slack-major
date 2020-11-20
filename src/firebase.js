c487794401e58a24a0da9ee49bfe1b9e49fc349d
parresh77 <parresh77@gmail.com>
2020-11-21 01:33:52 +0530

    Update firebase.js with firebase configuration details

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 const firebaseConfig =
 {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyAvN2m8JqFjTVf1nxJdAOM9rwrfGMiVqZE",
+    authDomain: "slack-major.firebaseapp.com",
+    databaseURL: "https://slack-major.firebaseio.com",
+    projectId: "slack-major",
+    storageBucket: "slack-major.appspot.com",
+    messagingSenderId: "887199674276",
+    appId: "1:887199674276:web:4f6c4420ed4b1f3621db31"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -18,5 +18,3 @@ const provider = new firebase.auth.GoogleAuthProvider();
 
 export {auth, provider};
 export default db;
-
-//The blank strings will be replaced by Environment Variables when the application is deployed
